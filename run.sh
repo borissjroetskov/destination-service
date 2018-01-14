2a8fe3614da559fa7007cdd9473f8256751c0849
Fabian Poels <fabian.poels@gmail.com>
2018-01-15 00:21:21 +0100

    update

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting License Server"
+echo "********************************************************"
+java -jar /usr/local/destination-service/destination-service-0.0.1.jar
