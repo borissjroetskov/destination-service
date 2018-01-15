3a6eec30f47daae00d824ec7bda72468f256148c
Fabian Poels <fabian.poels@gmail.com>
2018-01-15 23:10:59 +0100

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-echo "********************************************************"
-echo "Starting License Server"
-echo "********************************************************"
 java -jar /usr/local/destination-service/destination-service-0.0.1.jar
--- src/main/java/com/fabianpoels/destination/DestinationController.java
+++ src/main/java/com/fabianpoels/destination/DestinationController.java
@@ -1,11 +1,17 @@
 package com.fabianpoels.destination;
 
 import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/destinations")
@@ -26,4 +32,11 @@ public class DestinationController {
 		JetstreamDB.instance().root().generateDestination();
 		return "generated!";
 	}
+	
+	@PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<?> create(@RequestBody Destination d) {
+		JetstreamDB.instance().root().addDestination(d);;
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
